3706c57907c7430b8d2aab4b792f3f0f3825393f
PhilippMatthes <github@philippmatth.es>
2018-08-10 15:13:06 +0200

    Initial Commit

---
+++ mensa-tu-dresden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mensa-tu-dresden
+//
+//  Created by Philipp Matthes on 10.08.18.
+//  Copyright © 2018 Philipp Matthes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
